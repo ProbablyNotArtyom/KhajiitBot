f60e50795bca991178ae7b5df5f6906c121f0db1
Carson Herrington <herringtoncarson@gmail.com>
2026-01-24 00:47:10 -0500

    Migrated to official OpenAI gem Added k.dalle for image generation

--- src/OpenAI.rb
+++ src/OpenAI.rb
@@ -32,21 +32,22 @@ require 'openai'
 #====================================================================================================
 
 OPENAI_KEY = File.read("./ext/sys/openai").chomp	# OpenAI API key
-OpenAI.configure do |config|						# Configure client
-	config.access_token = OPENAI_KEY
-	config.log_errors = true
-end
-
-gpt_client = OpenAI::Client.new
+gpt_client = OpenAI::Client.new(
+	api_key: OPENAI_KEY
+)
 
 #====================================================================================================
 
-OPENAI_TEMPURATURE = 1.0  		# 0 to 2, higher number means more random
-OPENAI_MODEL = "gpt-5-nano"		# Model name
+OPENAI_TEMPERATURE = 1.0  			# 0 to 2, higher number means more random
+OPENAI_TEXT_MODEL = "gpt-5-nano"	# Text model name
+OPENAI_TEXT_PROMPT_MAX = 75			# Maximum input words for text prompts
+OPENAI_IMAGE_MODEL = "dalle-e-2"	# Image model name
+OPENAI_IMAGE_PROMPT_MAX = 150		# Maximum input words for image prompts
 
+# ASK Command
 $bot.command(:'ask') do |event, *prompt|
 	# Dont let people just flood the bot with requests to waste tokens
-	if (OpenAI.rough_token_count(prompt.join(" ")) > 75)
+	if (prompt.size > OPENAI_TEXT_PROMPT_MAX)
 		return event.channel.send_embed do |embed|
 			embed.title = "Error"
 			embed.description = "stop wasting my tokens headass"
@@ -64,18 +65,52 @@ $bot.command(:'ask') do |event, *prompt|
     end
 
 	# Query the API with the prompt
-	response = gpt_client.chat(
-		parameters: {
-			model: OPENAI_MODEL,
-			messages: [{ role: "user", content: "#{prompt.join(" ")}"}],
-			temperature: OPENAI_TEMPURATURE,
-		}
+	response = gpt_client.chat.completions.create(
+		model: OPENAI_TEXT_MODEL,
+		messages: [{ role: "user", content: prompt.join(" ")}],
+		temperature: OPENAI_TEMPERATURE
 	)
 
 	indicator_thread.kill	# Stop the typing indicator now that we have a response
 
 	return event.channel.send_embed do |embed|
-		embed.description = response.dig("choices", 0, "message", "content")
+		embed.description = response.choices[0].message.content
+		embed.color = EMBED_MSG_COLOR
+	end
+end
+
+# DALLE Command
+$bot.command(:'dalle') do |event, *prompt|
+	# Dont let people just flood the bot with requests to waste tokens
+	if (prompt.size > OPENAI_IMAGE_PROMPT_MAX)
+		return event.channel.send_embed do |embed|
+			embed.title = "Error"
+			embed.description = "stop wasting my tokens headass"
+			embed.color = EMBED_ERROR_COLOR
+		end
+	end
+
+	# Start a thread to send typing indicator heartbeats continuously until we kill it
+	indicator_thread = Thread.new do
+		loop do
+			event.channel.start_typing
+			DEBUG_PUTS "sending typing indicator"
+			sleep(4)
+		end
+    end
+
+	# Query the API with the prompt
+	response = gpt_client.images.generate(
+		prompt: prompt.join(" "),
+		model: "dall-e-2",
+		size: "1024x1024"
+	)
+
+	indicator_thread.kill	# Stop the typing indicator now that we have a response
+
+	return event.channel.send_embed do |embed|
+		embed.title = "\"#{prompt.join(" ")}\""
+		embed.image = Discordrb::Webhooks::EmbedImage.new(url: response.data[0].url)
 		embed.color = EMBED_MSG_COLOR
 	end
 end
